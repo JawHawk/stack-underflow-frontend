8d62d8a11ba1e1b663b4ab5c44b7b9f898fc7d62
Om Thakare <omthakare1606@gmail.com>
2023-01-22 01:56:32 +0530

    dashboard added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Dashboard from './Dashboard';
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div>
       <Navbar></Navbar>
       <div className="tag">All Your <span>stacks</span> under one place</div>
-      
-      
+      <Link to="./Dashboard"><button>Dashboard</button>
+      </Link>
     </div>
   );
 }
