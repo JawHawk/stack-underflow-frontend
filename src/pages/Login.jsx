fbf9656dceff6376c970a25387005c794184be2f
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-01-22 00:27:42 +0530

    login+signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,9 +7,20 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+function handleChange(e){
+  e.preventDefault();
+  if(e.target.name === "email"){
+    setEmail(e.target.value)
+  }
+  else{
+    setPassword(e.target.value)
+  }
 
-  function login() {
-    console.log(email);
+}
+  function login(e) {
+    e.preventDefault()
+    console.log(email,password);
+    
   }
   return (
 
@@ -18,11 +29,11 @@ const Login = () => {
       <form className="form">
         <Link to="/"><button className="close">Close</button></Link>
         <h1>LogIn</h1>
-        Email : <input type="text" placeholder="Enter your Email" className="input" onChange={(e) => setEmail( e.target.value)} />
+        Email : <input type="text" placeholder="Enter your Email" className="input" name="email" onChange={handleChange}/>
 
         <br />
         <br />
-        Password : <input type="password" placeholder="Enter your Password" className="input" onChange={(e) => setPassword(e.target.value)} />
+        Password : <input type="password" placeholder="Enter your Password" className="input" name="password" onChange={handleChange} />
         <br />
 
         <button onClick={login}>submit</button>
