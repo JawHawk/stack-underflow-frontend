e09ccfe6a48d1b2a465ed2765610103ba122c267
Tushar Jain <jain0735@gmail.com>
2023-01-26 10:07:53 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,44 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import logo from "../requireddata/logo.png";
+import "../requireddata/navbar.css"
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const loginDetail = localStorage.getItem("auth-token");
+  const handleClick = () => {
+    localStorage.removeItem("auth-token");
+    navigate("/login");
+  };
   return (
-    <div style={{backgroundColor : "transparent"}}>
-      <nav className="navbar navbar-expand-lg bg-transparent">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Stack UnderFlow</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-       {!localStorage.getItem("auth-token")?
-       <div className='d-flex justify-content-end'>
-        <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/signup">Sign up</Link>
-        </li>
-        </div>:
-        <div className='d-flex'>
-        <li className="nav-item">
-          <button className='btn btn-info' onClick={()=>navigate("/profile")}>Profile</button>
-        </li>
-        <li className="nav-item mx-2">
-          <button className="btn btn-info" aria-current="page" onClick={()=> {localStorage.removeItem("auth-token"); navigate("/login")}}>Log out</button>
-        </li>
+    <div className="nav-og">
+      <ul className="nav">
+        <div className="logo">
+          <img src={logo}></img>
+          <Link to="/" id="name">
+            Stack UnderFlow
+          </Link>
         </div>
-        }
+        {loginDetail ? (
+          <div className="log-sign">
+            <Link to="/Profile">
+              <button>Profile</button>
+            </Link>{" "}
+            <button onClick={handleClick}> Logout</button>
+          </div>
+        ) : (
+          <div className="log-sign">
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+            <Link to="/signup">
+              <button>Sign up</button>
+            </Link>
+          </div>
+        )}
       </ul>
-
     </div>
-  </div>
-</nav>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
